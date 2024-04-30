7ceaeba97a9c3995ffb75d12f7752779ed31036f
ElmarQarayev1 <elmareg@code.edu.az>
2024-04-30 07:02:44 +0400

    homework

---
+++ MvcPustok/MvcPustok/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MvcPustok.Areas.Manage.ViewModels;
+using MvcPustok.Data;
+using MvcPustok.Models;
+
+namespace MvcPustok.Areas.Manage.Controllers
+{
+	[Area("manage")]
+	public class SliderController:Controller
+	{
+		private readonly AppDbContext _context;
+
+		public SliderController(AppDbContext context)
+		{
+			_context = context;
+		}
+		public  IActionResult Index(int page=1)
+		{
+			var query = _context.Sliders;
+			return View(PaginatedList<Slider>.Create(query,page,2));
+		}
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Slider slider)
+		{
+            if (!ModelState.IsValid)
+            {
+                return View(slider);
+            }
+            if (_context.Sliders.Any(x => x.Order == slider.Order))
+			{
+				ModelState.AddModelError("Order", "Order already exists!");
+				return View(slider);
+			}
+			_context.Sliders.Add(slider);
+			_context.SaveChanges();
+			return RedirectToAction("index");
+		}
+	}
+}
+
---
+++ MvcPustok/MvcPustok/Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcPustok.Models
+{
+	public class Genre:BaseEntity
+	{
+        [MaxLength(20)]
+        [MinLength(3)]
+        [Required]
+        public string Name { get; set; }
+
+        public List<Book>? Books { get; set; }
+
+
+	}
+}
+
